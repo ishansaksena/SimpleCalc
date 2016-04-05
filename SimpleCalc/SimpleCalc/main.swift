46fcff726d6c061f147a03a5f754defb066dbd47
Ishan Saksena <ishansaksena@gmail.com>
2016-04-05 15:41:46 -0700

    Added files via upload

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -5,18 +5,19 @@
 //  Created by ishansaksena on 4/2/16.
 //  Copyright © 2016 ishansaksena. All rights reserved.
 //
+//  With extra credit option #1 implemented 
 
 import Foundation
 
 // Calculates the final result
-func result(n: [Int], oper: String) -> Int {
+func result(n: [Double], oper: String) -> Double {
     switch(oper) {
     case "+": return n[0] + n[1];
     case "-": return n[0] - n[1];
     case "*": return n[0] * n[1];
     case "/": return n[0] / n[1];
-    case "count": return n.count;
-    case "avg": return sum(n) / n.count;
+    case "count": return Double(n.count);
+    case "avg": return sum(n) / Double(n.count);
     case "fact": return fact(n[0]);
     default: return -1;
     }
@@ -39,8 +40,8 @@ func isNumber(oper: String) -> Bool {
 }
 
 // Returns the number of elements in the arra
-func sum(numbers: [Int]) -> Int {
-    var n = 0
+func sum(numbers: [Double]) -> Double {
+    var n = 0.0
     for number in numbers {
         n += number
     }
@@ -48,13 +49,13 @@ func sum(numbers: [Int]) -> Int {
 }
 
 // Returns the factorial of the number passed in
-func fact(number: Int) -> Int {
+func fact(number: Double) -> Double {
     if (number == 0) {
         return 1
     }
-    var r = 1
-    for n in 1...number {
-        r *= n
+    var r = 1.0
+    for n in 1...Int(number) {
+        r *= Double(n)
     }
     return r
 }
@@ -62,13 +63,13 @@ func fact(number: Int) -> Int {
 print("Enter an expression separated by returns")
 
 var current = "1"
-var numbers = [Int]()
+var numbers = [Double]()
 var oper = ""
 repeat {
     current = readLine(stripNewline: true)!
     // Add to the array if it is a number
     if(isNumber(current)) {
-        numbers.append(Int.init(current)!)
+        numbers.append(Double.init(current)!)
     } else {// Update the operand
         oper = current
     }
@@ -76,7 +77,7 @@ repeat {
 
 // Get one last number if it's one of the binary operations
 if (oper == "+" || oper == "-" || oper == "*" || oper == "/") {
-    numbers.append(Int.init(readLine(stripNewline: true)!)!)
+    numbers.append(Double.init(readLine(stripNewline: true)!)!)
 }
 //Testing only
 //print("The array is \(numbers) and the operation is \(oper)")
